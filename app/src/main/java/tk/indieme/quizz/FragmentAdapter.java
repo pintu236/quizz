e77d737be90dff05e3058836fc8e2b20b7e26c2c
simon_qualwebs <simon.qualwebs@gmail.com>
2019-06-02 21:45:24 +0530

    1.Added SignUp Screen 2.Added Login Screen 3.Added ViewPager for Login,Sign up,Welcome

---
+++ app/src/main/java/tk/indieme/quizz/FragmentAdapter.java
@@ -0,0 +1,35 @@
+package tk.indieme.quizz;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    private int tabCount;
+
+    public FragmentAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new WelcomeFragment();
+            case 1:
+                return new SignupFragment();
+            case 2:
+                return new LoginFragment();
+            default:
+                return new WelcomeFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
